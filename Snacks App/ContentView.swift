618c2df87835e65633937d06dc6d4124c40c7336
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2025-08-06 19:40:48 +0100

    Initial Commit

---
+++ Snacks App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snacks App
+//
+//  Created by Luiz Correa on 06/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snacks App/Snacks_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snacks_AppApp.swift
+//  Snacks App
+//
+//  Created by Luiz Correa on 06/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Snacks_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
